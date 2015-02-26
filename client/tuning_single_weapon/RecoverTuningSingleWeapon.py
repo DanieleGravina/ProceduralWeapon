add3849b5f3611f08996e309a01a4bb4cdcf79a0
DanieleGravina <daniele.gravina@mail.polimi.it>
2015-02-26 09:10:18 +0100

    Added recover tuning single weapon

---
+++ client/tuning_single_weapon/RecoverTuningSingleWeapon.py
@@ -0,0 +1,579 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+import random
+import matplotlib.pyplot as plt
+import numpy
+import pickle
+import time
+import threading
+
+
+from decimal import *
+from functools import partial
+from operator import attrgetter
+
+
+from deap import base
+from deap import creator
+from deap import tools
+
+#####################################################################
+
+#################
+#GA PARAMETERS###
+#################
+
+
+# size of the population
+NUM_POP = 50
+
+#:param mu: The number of individuals to select for the next generation.
+#:param lambda\_: The number of children to produce at each generation.
+LAMBDA = NUM_POP
+MU = NUM_POP
+
+#crossover (0.6 -1), mutation prob (1/nreal)
+#crossover probability, mutation probability, number of generation
+CXPB, MUTPB, NGEN = 0.6, 0.1, 50 #160 min
+
+#eta c (5-50), eta_m (5-20)
+ETA_C, ETA_M = 20, 20
+
+weight_obj_1 = MINIMIZE
+
+path = str(NUM_POP) + "_pop_" + str(NGEN) + "_iter_simulated_binary_new2_1"
+
+
+N_CYCLES = 2
+
+#####################################################################
+
+creator.create("FitnessMax", base.Fitness, weights = (1.0, weight_obj_1))
+creator.create("Individual", list, fitness = creator.FitnessMax)
+
+#initialization
+
+toolbox = base.Toolbox()
+
+toolbox.register("individual", tools.initCycle, creator.Individual, [lambda : 0 for _ in range(10)], n = 1)
+
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+stats1 = tools.Statistics(key = lambda ind: ind.fitness.values[0])
+
+stats1.register("avg", numpy.mean)
+stats1.register("std", numpy.std)
+stats1.register("min", numpy.min)
+stats1.register("max", numpy.max)
+
+stats2 = tools.Statistics(key = lambda ind: ind.fitness.values[1])
+
+stats2.register("avg", numpy.mean)
+stats2.register("std", numpy.std)
+stats2.register("min", numpy.min)
+stats2.register("max", numpy.max)
+
+mstats = tools.MultiStatistics(entropy = stats1, diff = stats2)
+
+logbook = tools.Logbook()
+
+hof = tools.ParetoFront()
+
+def printWeapon(pop):
+    i = 0
+    for ind in pop :
+        print("(" + str(i) + ")")
+        i += 1
+        print("Weapon "+ " Rof:" + str(ind[0]) + " Spread:" + str(ind[1]) + " MaxAmmo:" + str(ind[2]) 
+            + " ShotCost:" + str(ind[3]) + " Range:" + str(ind[4]) )
+        print("Projectile "+ " Speed:" + str(ind[5]) + " Damage:" + str(ind[6]) + " DamageRadius:" + str(ind[7])
+            + " Gravity:" + str(ind[8]) + " Explosive:" + str(ind[9]) )
+
+        print("fitness: " + str(ind.fitness.values))
+        print("*********************************************************")
+
+def writeWeapon(pop, pop_file):
+    i = 0
+    for ind in pop :
+        pop_file.write("(" + str(i) + ")\n")
+        i += 1
+        pop_file.write("Weapon "+ " Rof:" + str(ind[0]) + " Spread:" + str(ind[1]) + " MaxAmmo:" + str(ind[2]) 
+            + " ShotCost:" + str(ind[3]) + " Range:" + str(ind[4]) + "\n")
+        pop_file.write("Projectile "+ " Speed:" + str(ind[5]) + " Damage:" + str(ind[6]) + " DamageRadius:" + str(ind[7])
+            + " Gravity:" + str(ind[8]) + " Explosive:" + str(ind[9]) +"\n")
+
+        pop_file.write("fitness: " + str(ind.fitness.values) + "\n")
+        pop_file.write("*********************************************************" + "\n")
+    pop_file.write("\n" + "============================================================================================================" + "\n")
+
+def check_param(param, min, max):
+    if param < min :
+        param = min
+    elif param > max :
+        param = max
+
+
+    param = float("{0:.2f}".format(param))
+    return param
+
+def check(param, n) :
+
+    return check_param(param, limits[n][0], limits[n][1])
+
+def checkBounds(min, max):
+    def decorator(func):
+        def wrapper(*args, **kargs):
+            offspring = func(*args, **kargs)
+            for child in offspring:
+                for i in range(len(child)):
+                    child[i] = check(child[i], i)
+            return offspring
+        return wrapper
+    return decorator
+
+def initialize_server(PORT):
+    if DEBUG:
+        return
+
+    clients = []
+
+    for i in range(NUM_SERVER):
+        clients.append(BalancedWeaponClient(PORT[i]))
+
+    for c in clients :
+        c.SendInit()
+        c.SendStartMatch()
+        c.SendClose()
+
+# Run the simulation on the server side (UT3)
+def simulate_population(population, port) :
+
+    stats = {}
+    pop = {}
+    threads = []
+
+    if DEBUG:
+        return stats, port
+
+    num_server = len(port)
+    
+    lock = threading.Lock()
+
+    for i in range(len(population)):
+
+        if not population[i].fitness.valid :
+            pop.update( {i*2 : population[i] + Weapon_Fixed} )
+
+    while len(pop) != 0 :
+
+        for i in range(num_server):
+            threads.append( SimulationThread(pop, i, port, lock) )
+
+        for t in threads:
+            t.start()
+
+        for t in threads:
+            stats.update(t.join())
+
+        del threads
+
+        threads = []
+
+        num_server = len(port)
+
+    return stats, port
+
+def match_suicides(index, statistics) :
+    suicides = 0
+    suicides += statistics[index][1] - statistics[index + 1][0]
+    #suicides += statistics[index + 1][1] - statistics[index][0]
+
+    return suicides
+
+
+def match_kills(index, statics) :
+
+    total_kills = 0
+
+    for key, val in statics.items():
+        if key >= index and key <= index + (NUM_BOTS - 1) :
+            total_kills += val[0]
+
+    total_kills = total_kills/GOAL_SCORE
+
+    return total_kills
+
+def difference(index, pop) :
+
+    total = 0
+    diff = 0
+
+    for j in range(10):
+        norm1 = (pop[index][j] - limits[j][0])/(limits[j][1] - limits[j][0])
+        norm2 = (Weapon_Target[j] - limits[j][0])/(limits[j][1] - limits[j][0])
+        total = norm1 - norm2
+        diff += pow(norm1 - norm2, 2)
+
+    return sqrt(diff)
+
+
+
+def entropy(index, statistics) :
+
+    e = 0
+
+    total_kills = 0
+    total_dies = 0
+
+    for key, val in statistics.items():
+        if key >= index and key <= index + (NUM_BOTS - 1) :
+            total_kills += val[0]
+            total_dies += val[1]
+
+    for i in range(index, index + NUM_BOTS):
+        e += evaluate_entropy(i, statistics, total_kills, total_dies, NUM_BOTS)
+
+    print(str(index) + " entropy " + str(e) +
+             " kills " + str(total_kills/GOAL_SCORE) + " suicides " + str(pow(9/10, match_suicides(index, statistics))))
+
+    return e + total_kills/GOAL_SCORE + pow(9/10, match_suicides(index, statistics))
+
+
+def evaluate_entropy(index, statistics, total_kills, total_dies, N) :
+
+    kills = statistics[index][0]
+
+    p_kills = 0 if total_kills == 0 else kills/total_kills
+
+    entropy_kill = p_kills*log(p_kills, N) if p_kills != 0 else 0
+
+    return -entropy_kill
+
+# ATTENTION, you MUST return a tuple
+def evaluate(index, population, statics):
+
+    if DEBUG:
+        return random.randint(0,2), difference(index, population)
+
+    e = entropy(index*2, statics)
+    diff = difference(index, population)
+    
+    print('entropy :' + str(index) + " " + str(e))
+    print('difference :' + str(index) + " " + str(diff))
+
+    return e, diff
+
+def customCrossover(ind1, ind2):
+
+    return tools.cxSimulatedBinaryBounded(ind1,
+                                            ind2,
+                                            eta = ETA_C, 
+                                            low  = [limits[j][0] for j in range(10)], 
+                                            up = [limits[j][1] for j in range(10)])
+
+
+toolbox.register("mate", customCrossover)
+
+
+toolbox.register("mutate", tools.mutPolynomialBounded, eta = ETA_M,
+                                                       low  = [limits[j][0] for j in range(10)], 
+                                                       up = [limits[j][1] for j in range(10)],
+                                                       indpb = 1/NUM_PAR)
+
+
+toolbox.register("select", tools.selNSGA2)
+
+toolbox.decorate("mate", checkBounds(0,1))
+toolbox.decorate("mutate", checkBounds(0,1))
+
+toolbox.register("evaluate", evaluate)
+
+stats1 = tools.Statistics(key = lambda ind: ind.fitness.values[0])
+
+stats1.register("avg", numpy.mean)
+stats1.register("std", numpy.std)
+stats1.register("min", numpy.min)
+stats1.register("max", numpy.max)
+
+stats2 = tools.Statistics(key = lambda ind: ind.fitness.values[1])
+
+stats2.register("avg", numpy.mean)
+stats2.register("std", numpy.std)
+stats2.register("min", numpy.min)
+stats2.register("max", numpy.max)
+
+mstats = tools.MultiStatistics(entropy = stats1, diff = stats2)
+
+logbook = tools.Logbook()
+
+hof = tools.ParetoFront()
+
+def binary_tournament(ind1, ind2):
+        if ind1.fitness.dominates(ind2.fitness):
+            return ind1
+        elif ind2.fitness.dominates(ind1.fitness):
+            return ind2
+
+        if ind1.fitness.crowding_dist < ind2.fitness.crowding_dist:
+            return ind2
+        elif ind1.fitness.crowding_dist > ind2.fitness.crowding_dist:
+            return ind1
+
+        if random.random() <= 0.5:
+            return ind1
+        return ind2
+
+def eaMuPlusLambda(pop, gen, port, logbook_file) :
+    offspring = []
+
+    # Vary the population
+    offspring = []
+
+    while len(offspring) < LAMBDA:
+        ind1, ind2 = random.sample(pop, 2)
+        ind = binary_tournament(ind1, ind2)
+        offspring.append(ind)
+
+
+    offspring = [toolbox.clone(ind) for ind in offspring]
+    
+    for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
+        if random.random() <= CXPB:
+            toolbox.mate(ind1, ind2)
+            del ind1.fitness.values, ind2.fitness.values
+
+        temp1 = toolbox.clone(ind1)
+        temp2 = toolbox.clone(ind2)
+        
+        toolbox.mutate(ind1)
+        toolbox.mutate(ind2)
+
+        if ind1.fitness.valid:
+            for i in range(NUM_PAR):
+                if temp1[i] != ind1[i]:
+                    del ind1.fitness.values
+                    break
+
+        if ind2.fitness.valid:
+            for i in range(NUM_PAR):
+                if temp2[i] != ind2[i]:
+                    del ind2.fitness.values
+                    break
+        
+
+
+    statistics, port = simulate_population(offspring, port)
+
+    logbook_file.write("Gen : " + str(gen+1) + "\n")
+    for key, val in statistics.items():
+            logbook_file.write(str(key) + " : " + str(val) + "\n")
+    logbook_file.write("*********************************************************\n")
+
+    # Evaluate only invalid population
+    index = 0
+    fit = 0,
+    len_invalid = 0
+
+    fitnesses = [0 for _ in range(len(offspring))]
+
+    for index in range(len(offspring)):
+
+        if not offspring[index].fitness.valid :
+            fit = toolbox.evaluate(index, offspring, statistics)
+            fitnesses[index] = fit
+            len_invalid += 1
+        else :
+            fitnesses[index] = offspring[index].fitness.values
+
+    for ind, fit in zip(offspring, fitnesses):
+        ind.fitness.values = fit
+
+    pop[:] = toolbox.select(pop + offspring, MU)
+
+    return pop, port, len_invalid
+
+
+
+
+
+def getPOP(pop, content):
+
+    line_readed = 0
+    index = 0
+    temp = []
+
+    for string in content:
+        
+        if "Weapon" in string or "Projectile" in string:
+            split_spaces = string.split(" ")
+
+            for splitted in split_spaces:
+                if ":" in splitted:
+                    split_colon = splitted.split(":")
+                    temp += [float(split_colon[1])]
+                    if (len(temp) == 10):
+                        for i in range(10):
+                            pop[index][i] = temp[i]
+                        temp = []
+
+        if "fitness" in string :
+            split_spaces = string.split(" ")
+
+            for splitted in split_spaces:
+                if "(" in splitted and not ")" in splitted:
+                    splitted = splitted.replace("(", "")
+                    splitted = splitted.replace(")", "")
+                    splitted = splitted.replace(",", "")
+                    splitted = splitted.replace("\n", "")
+                    fit = float(splitted)
+                    temp_fit = fit
+
+                if ")" in splitted and not "(" in splitted:
+                    splitted = splitted.replace("(", "")
+                    splitted = splitted.replace(")", "")
+                    splitted = splitted.replace(",", "")
+                    splitted = splitted.replace("\n", "")
+                    fit = float(splitted)
+                    pop[index].fitness.values = temp_fit, fit
+
+            index += 1
+
+            if index == NUM_POP:
+                return pop, content[line_readed + 1:]
+
+        line_readed += 1
+
+
+
+def main():
+
+	os.chdir(path)
+
+    logbook_file = open("logbook_final.txt", "a")
+    pop_file = open("population.txt", "a")
+    best_ind = open("best.txt", "w")
+    population_cluster = open("population_cluster.txt", "w")
+
+    content = pop_file.readlines()
+
+    pop = toolbox.population(NUM_POP)
+
+    pop, content = getPOP(pop, content, True)
+
+    for g in range(29):
+
+        pop, content = getPOP(pop, content, False)
+
+        record = mstats.compile(pop)
+        hof.update(pop)
+
+        logbook.record(gen = g, **record)
+
+    logbook.header = "gen", "entropy", "diff"
+    logbook.chapters["entropy"].header = "min", "avg", "max"
+    logbook.chapters["diff"].header = "min", "avg", "max"
+
+    print(logbook)
+
+    initialize_server(port)
+
+    gen = 29
+
+    for g in range(gen, NGEN):
+
+        pop, port, len_invalid = eaSimple(pop, g, port, logbook_file)
+
+        printWeapon(pop)
+        writeWeapon(pop, pop_file)
+
+        record = stats.compile(pop)
+        hof.update(pop)
+
+        logbook.record(gen = g, evals = len_invalid, **record)
+
+        print(logbook)
+
+    log_string = str(logbook)
+
+    logbook_file.write(log_string)
+
+    writeWeapon(pop, pop_file)
+
+    writeWeapon(pop, population_cluster)
+
+    best_ind.write("Best individuals:\n")
+    writeWeapon(hof, best_ind)
+
+    best_ind.close()
+
+    pop_file.close()
+
+    #################################################################################
+
+    ############################################
+    ###plot min, avg, max of singles objectives#
+    ############################################
+
+    plt.figure(figsize=(16,9))
+
+    plt.subplot(211)
+
+    gen = logbook.select("gen")
+    fit_avg = logbook.chapters["entropy"].select("avg")
+    fit_max = logbook.chapters["entropy"].select("max")
+    fit_min = logbook.chapters["entropy"].select("min")
+
+    plt.plot(gen, fit_avg, 'r--', gen, fit_max, 'b-', gen, fit_min, 'g')
+
+    plt.xlabel("Generation")
+    plt.ylabel("Entropy")
+
+    plt.subplot(212)
+
+    fit_avg = logbook.chapters["diff"].select("avg")
+    fit_max = logbook.chapters["diff"].select("max")
+    fit_min = logbook.chapters["diff"].select("min")
+
+    plt.plot(gen, fit_avg, 'r--', gen, fit_max, 'b-', gen, fit_min, 'g')
+
+    plt.xlabel("Generation")
+    plt.ylabel("Difference Target Weapon")
+
+    plt.savefig("graph.png", bbox_inches='tight')
+
+    ##########################################################################
+
+    ##########################
+    ##plot multiobjective ####
+    ##########################
+
+    plt.figure(figsize=(16,9))
+
+    e = [pop[i].fitness.values[0] for i in range(len(pop))]
+    d = [pop[i].fitness.values[1] for i in range(len(pop))]
+
+    e_front = [hof[i].fitness.values[0] for i in range(len(hof))]
+    d_front = [hof[i].fitness.values[1] for i in range(len(hof))]
+
+    plt.xlabel("Entropy")
+    plt.ylabel("Difference")
+
+    plt.scatter(e, d)
+    plt.scatter(e_front, d_front, c=u'r')
+    plt.plot(e_front, d_front)
+
+    plt.savefig("pareto.png", bbox_inches='tight')
+    ##########################################################################
+
+if __name__ == '__main__':
+    main()
+
+	
+
+
+
+
+
